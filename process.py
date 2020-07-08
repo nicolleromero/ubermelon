944835f28117398c1993ec6c391ab6734025e2ad
Nicolle Romero <nicollearomero@gmail.com>
2020-07-07 17:21:10 -0700

    Updated day in process script, added comments

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") # accessing data from the server stored in this file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # function to generate a sales report from a log file
+    for line in log_file: # iterate over the entries in the log
+        line = line.rstrip() # remove white space from each line on the right
+        day = line[0:3] 
+        if day == "Mon": # if the day in the line matches this day, select it
+            print(line) # now print that line
 
 
-sales_reports(log_file)
+sales_reports(log_file) # call the function to generate the sales report
