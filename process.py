edb6fe5d20f200b701e0492e0b7b64a8d7154e07
Nicolle Romero <nicollearomero@gmail.com>
2020-07-07 17:40:58 -0700

    Updated comments

--- process.py
+++ process.py
@@ -3,8 +3,8 @@ log_file = open("um-server-01.txt") # accessing data from the server stored in t
 
 def sales_reports(log_file): # function to generate a sales report from a log file
     for line in log_file: # iterate over the entries in the log
-        line = line.rstrip() # remove white space from each line on the right
-        day = line[0:3] 
+        line = line.rstrip() # remove trailing spaces from each line on the right
+        day = line[0:3] # declare the variable day and set it equal to the first three characters in the line
         if day == "Mon": # if the day in the line matches this day, select it
             print(line) # now print that line
 
