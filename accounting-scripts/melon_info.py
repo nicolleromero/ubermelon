a59a70b69f7109cab94815537292cf9b7fdca630
Nicolle Romero <nicollearomero@gmail.com>
2020-07-13 18:23:14 -0700

    Updates to melon dict script

---
+++ accounting-scripts/melon_info.py
@@ -0,0 +1,36 @@
+"""Print out all the melons in our inventory."""
+
+
+# from melons import melon_names, melon_seedlessness, melon_prices
+
+
+# def print_melon(name, seedless, price):
+#     """Print each melon with corresponding attribute information."""
+
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
+
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+# New script
+
+from melons import melon_inventory
+
+
+def print_melon_new(melon_inventory):
+
+    for melon in melon_inventory.items():
+
+        print(f"{melon[0].upper()}")
+
+        for k, v in melon[1].items():
+            print(f"     {k}: {melon[1][k]}")
+
+print_melon_new(melon_inventory)
+
+
---
+++ accounting-scripts/melons.py
@@ -0,0 +1,75 @@
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
+
+melon_inventory = {
+
+    'Honeydew': {
+        'seedless': True,
+        'price': 0.99,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+
+    'Crenshaw': {
+        'seedless': False,
+        'price': 2.00,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+
+    'Crane': {
+        'seedless': False,
+        'price': 2.50,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+
+    'Casaba': {
+        'seedless': False,
+        'price': 2.50,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+
+    'Cantaloupe': {
+        'seedless': False,
+        'price': 0.99,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+
+}
+
+def add_info(inventory, new_category):
+
+    inventory.setdefault(new_category, None)
+
+    return dict
+
+
