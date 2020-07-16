7c334c049c86496e43e98503885d61d84cc8a3d3
Nicolle Romero <nicollearomero@gmail.com>
2020-07-16 04:08:36 +0000

    Updates to raffle script

--- melon-raffle/version2/raffle.py
+++ melon-raffle/version2/raffle.py
@@ -1,6 +1,26 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from customers import Customer
+from customers import get_customers_from_file
+import random
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    print("Tell {name} at {email} that they've won!".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+    ))
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+
+run_raffle()
